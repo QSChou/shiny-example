18f8c2c07d47eea19d73165e4a8c70d92fddf1d4
QS Chou <qs.chou@gmail.com>
2016-09-10 11:29:17 +0800

    Create server.R

---
+++ hello-iris/server.R
@@ -0,0 +1,19 @@
+shinyServer(function(input,output) {
+  y <- reactive(
+     {colnum <- as.numeric(input$colnum)
+      iris[,colnum]}
+  )
+  
+  output$t1 <- renderPrint(
+    names(iris)[as.numeric(input$colnum)])
+  output$summary <- renderPrint({
+                      summary(y())})
+ 
+  output$dist <- renderPlot(
+    {
+      x <- y()
+      bins <- seq(min(x),max(x),length.out = input$slider1)
+      hist(x,breaks = bins,col='blue',border = 'white')
+    }
+  )  
+})
