bea4178b93fd674e62bb2b9841ba27e68e07f715
QS Chou <qs.chou@gmail.com>
2016-09-12 01:48:08 +0800

    Update ui.R

--- hello-iris/ui.R
+++ hello-iris/ui.R
@@ -6,13 +6,10 @@ fluidPage(
       wellPanel(
     radioButtons("dataset", "Choose a dataset:", choices = c("iris", "pressure", "cars"))),
       wellPanel(
-    #selectInput("colnum", "Choose a Column Number:", choices = c(1,2,3,4))
         uiOutput("colui")
         ),
       wellPanel(
     sliderInput("bining", label = "Bining Numer", min = 0, max = 100 , value = 50))),
-    #wellPanel(
-    #  selectInput("colnum", "Choose a Column Number:", choices = c("cols"))),
     mainPanel(
       verbatimTextOutput("cols")
       ,verbatimTextOutput("coln")
